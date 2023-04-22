3031de2d4ba914bb3e673347920a38aa05a0f5d8
sarbjot-konectAI <sarbjot.s@konect.ai>
2023-04-21 18:08:23 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    const getData = async () => {
+      const data = await axios.get('http://localhost:5000/api/resumes');
+      console.log(data);
+    };
+    getData();
+  }, []   );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Simple React Typescript Tailwind Sample
+      </h1>
     </div>
   );
 }
